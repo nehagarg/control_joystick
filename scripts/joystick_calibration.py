78f7a7a469396b2e9593cffe6a75c06f9a9f4153
neha@rris-desktop <neha@rris-desktop>
2023-09-18 16:59:36 +0800

    Updated script to publish both Point and Twist

--- scripts/joystick_calibration.py
+++ scripts/joystick_calibration.py
@@ -1,5 +1,5 @@
 import rospy
-from geometry_msgs.msg import Vector3, Point
+from geometry_msgs.msg import Vector3, Point, Twist
 
 pub = rospy.Publisher('/py/joystick', Point, queue_size=5)
 
@@ -7,7 +7,8 @@ class CalibrateJoystick():
     def __init__(self, joystick_topic = '/py/joystick', raw_joystick_topic = '/arduino/joystick_raw'):
         self.joystick_topic = joystick_topic
         self.raw_joystick_topic = raw_joystick_topic
-        self.joystick_publisher = rospy.Publisher(joystick_topic, Point, queue_size=10)
+        self.joystick_publisher_point = rospy.Publisher(joystick_topic+"_point", Point, queue_size=10)
+        self.joystick_publisher_twist = rospy.Publisher(joystick_topic+"_twist", Twist, queue_size=10)
         rospy.Subscriber(raw_joystick_topic, Vector3, self.raw_joystick_cb)
         self.xMax = 952
         self.xMin = 8
@@ -27,6 +28,7 @@ class CalibrateJoystick():
     def raw_joystick_cb(self, raw_joystick_msg):
         
         joy_msg = Point()
+        joy_msg_twist = Twist()
         
         rawValue = [raw_joystick_msg.x, raw_joystick_msg.y]
         cValue = [0,0]
@@ -46,13 +48,15 @@ class CalibrateJoystick():
                 
         joy_msg.x = cValue[1]
         joy_msg.y = -1*cValue[0]
+
+        joy_msg_twist.linear.x = joy_msg.x
+        joy_msg_twist.angular.z = joy_msg.y
     
 
-    
 
 
-
-        self.joystick_publisher.publish(joy_msg)
+        self.joystick_publisher_point.publish(joy_msg)
+        self.joystick_publisher_twist.publish(joy_msg_twist)
 
 
 if __name__ == '__main__':
